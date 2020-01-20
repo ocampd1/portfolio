740e9566eff746f7a41e9066e370e786312fd94d
unknown <you@example.com>
2020-01-20 15:09:02 -0600

    WIP>

---
+++ web-portfolio/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AppHomeComponent} from "./components/app-home/app-home.component";
+
+
+const routes: Routes = [
+  {path: 'home', component: AppHomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
