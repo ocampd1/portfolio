ca815345afb00f2db77837c5809087fa9341c836
ocampd1 <ocampdesigns@gmail.com>
2022-05-18 02:31:50 -0700

    chore: created files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {AboutComponent} from "./components/about/about.component";
+import {WorkComponent} from "./components/work/work.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'work', component: WorkComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
+
+export const routingComponents = [
+  HomeComponent,
+  AboutComponent,
+  WorkComponent
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { WorkComponent } from './components/work/work.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavigationComponent } from './shared-components/navigation/navigation.component';
+import { BodyComponent } from './shared-components/body/body.component';
+import { FooterComponent } from './shared-components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    WorkComponent,
+    ContactComponent,
+    NavigationComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
